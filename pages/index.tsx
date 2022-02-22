e7961cda89f2ea343575d664ce85cb41ca7e917a
Carlos Rivera <me@carlosrivera.im>
2022-02-22 01:33:04 -0600

    💅 improved background

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="relative dark bg-[#101113] bg-opacity-50 overflow-hidden min-h-screen">
       <div className="mx-auto">
-        <div className="absolute inset-0 bg-bottom bg-no-repeat bg-[#101113] bg-opacity-50 backdrop">
+        <div className="absolute inset-0 bg-bottom bg-no-repeat bg-[#101113] bg-opacity-60 backdrop">
           <div className="absolute inset-0 bg-bottom main-mask" >
           </div>
         </div>
@@ -29,48 +29,60 @@ export default function Home() {
                 <span className="inline bg-clip-text bg-gradient-to-tr from-lime-500 to-lime-300">Carlos Rivera</span>
                 <span className="inline outline-title-400">, an</span>{' '}<br />
                 <span className="inline outline-title-b-400">Entrepreneur &amp; </span>{' '}
-                <span className="inline bg-clip-text bg-gradient-to-tr from-blue-400 via-indigo-500 to-indigo-500">Product Builder.</span>
+                <span className="inline bg-clip-text bg-gradient-to-tr from-blue-400 via-blue-500 to-indigo-500">Product Builder.</span>
               </h1>
-              <p className="lg:max-w-3xl my-3 text-base text-zinc-400 sm:my-5 sm:text-lg md:my-5 md:text-xl lg:mx-0">
+              <p className="lg:max-w-3xl my-6 text-base text-zinc-400 sm:my-8 sm:text-lg md:my-10 md:text-xl lg:mx-0">
                 Machine Learning &amp; Infrastructure Developer -- Cofunded <a className="text-fx text-zinc-200 font-bold" href="https://synx.ai" target="_blank" rel="noreferrer"><span className="underline underline-offset-4 decoration-1">synx.ai</span></a> -- Quant &amp; FinTech Enthusiast -- Previously at HP-Labs, Hydra-Technologies &amp; Microsoft.
               </p>
 
-              <p className="pt-8 pb-4 w-full flex flex-col md:flex-row items-center justify-between md:justify-start">
+              <p className="py-4 w-full flex flex-col md:flex-row items-center justify-between md:justify-start">
+                <a className="w-6/12 my-2 text-center md:w-2/12 md:pr-4" href="/Carlos Rivera - Resume 2022.pdf" target="_blank" rel="noreferrer">
+                  <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-lime-300 hover:bg-lime-300 hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 md:mr-4 inline-flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                      <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                    </svg>
+                    <span className="px-2 font-semibold">Resume</span>
+                  </button>
+                </a>
+                
                 <a className="w-6/12 my-2 text-center md:w-2/12 md:pr-4" href="https://github.com/carlosrivera" target="_blank" rel="noreferrer">
                   <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-zinc-200 hover:bg-zinc-200 hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 md:mr-4 inline-flex justify-center items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                       <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
                     </svg>
-                    <span className="px-2">GitHub</span>
-                  </button>
-                </a>
-                
-                <a className="w-6/12 my-2 md:w-2/12 md:pr-4" href="https://twitter.com/carlos_gif" target="_blank" rel="noreferrer">
-                  <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-[#08a0e9] hover:bg-[#08a0e9] hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 md:mr-4 inline-flex justify-center items-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                      <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
-                    </svg>
-                    <span className="px-2">Twitter</span>
+                    <span className="px-2 font-semibold">GitHub</span>
                   </button>
                 </a>
 
-                <a className="w-6/12 my-2 md:w-2/12" href="https://www.linkedin.com/in/jcarlosrivera/" target="_blank" rel="noreferrer">
-                  <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-[#0077B5] hover:bg-[#0077B5] hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 inline-flex justify-center items-center">
+                <a className="w-6/12 my-2 md:w-2/12 md:pr-4" href="https://www.linkedin.com/in/jcarlosrivera/" target="_blank" rel="noreferrer">
+                  <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-[#0077B5] hover:bg-[#0077B5] hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 md:mr-4 inline-flex justify-center items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                       <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                     </svg>
-                    <span className="px-2">LinkedIn</span>
+                    <span className="px-2 font-semibold">LinkedIn</span>
+                  </button>
+                </a>
+                
+                <a className="w-6/12 my-2 md:w-2/12" href="https://twitter.com/carlos_gif" target="_blank" rel="noreferrer">
+                  <button className="w-full text-center text-zinc-300 ring-zinc-400 hover:text-white hover:scale-105 transition-all duration-300 hover:ring-[#08a0e9] hover:bg-[#08a0e9] hover:bg-opacity-20 rounded-md ring-2 p-2 px-4 inline-flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                      <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
+                    </svg>
+                    <span className="px-2 font-semibold">Twitter</span>
                   </button>
                 </a>
               </p>
             </div>
 
             <div className="flex flex-wrap md:flex-nowrap justify-center my-8 md:inline-flex md:items-center md:mb-0">
-              <img src="/profile-sq.jpg" className="profile w-5/12 md:w-full md:max-w-xs h-auto rounded-full shadow-lg hover:rotate-6 hover:scale-110 transition-all duration-1000" alt="..." />
-              <p className="align-middle text-zinc-400 sm:text-md md:text-lg md:pl-16 mt-10">
+              <img src="/profile-sq.jpg" className="profile w-5/12 md:w-full md:max-w-xs h-auto rounded-full shadow-xl hover:rotate-3 hover:scale-110 transition-all duration-1000" alt="..." />
+              <p className="align-middle text-zinc-400 sm:text-md md:text-lg md:pl-16 md:mt-0 mt-10">
                 Passionate coder in love with <label className="text-white">tacos</label> and <label className="text-white">craft beer</label> (my DM&apos;s are always open for both), with an extensive 
                 experience in <label className="text-lime-400">Aerospace</label> (energy and fuel optimization, predictive maintenance, automatic routing planning), <label className="text-lime-400">Finance</label> (risk assestment, fraud detection, price forecasting), 
                 and <label className="text-lime-400">Deep Tech</label> (Machine Learning, 3D Programing, Blockchain).
+                <br /><br />
+                On my spare time, I enjoy <label className="text-white">sci-fi</label>, hiking, swiming, wood-working or pub-crawling with my dogs.
               </p>
             </div>
           </main>
