fb32d3c79b56efa7b6eec856db8d743920d1696b
carlosrivera <me@carlosrivera.im>
2022-02-22 03:25:37 +0000

    Deploying to gh-pages from @ carlosrivera/carlosrivera.github.io@b4da98352fad37183b8755d3bd28988778d13edb 🚀

---
+++ _next/static/sE28UcL3Wq0X6xI0i3Y6n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-23c56fbe6b25cc04.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
