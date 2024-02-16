6cfa4bf9cc25a354afc41a62403d6ef88bb5d384
Carlos <me@carlosrivera.im>
2024-02-16 10:55:55 -0500

    💅 remove scroll-bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,10 @@
 import '../styles/globals.css'
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Carlos Rivera - Product Builder",
+  description: "",
+};
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
